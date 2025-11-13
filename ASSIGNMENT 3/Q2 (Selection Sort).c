bf140246eaf85ff26e09e766b2ba27a0c0aceeb5
anushkah123 <hardikaranushka7@gmail.com>
2025-11-14 00:08:09 +0530

    Update Q2 (Selection Sort).c

--- ASSIGNMENT 3/Q2 (Selection Sort).c
+++ ASSIGNMENT 3/Q2 (Selection Sort).c
@@ -33,3 +33,38 @@ int main() {
 
     return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Selection sort (simple compare i and j)
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[i]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
