5fb5b2b992616685b2ed1111aac399e87616ca3a
anushkah123 <hardikaranushka7@gmail.com>
2025-11-13 21:19:02 +0530

    Update Q1.c

--- ASSIGNMENT 12/Q1.c
+++ ASSIGNMENT 12/Q1.c
@@ -17,18 +17,19 @@ void insertEdge(int arr[V][V], int i, int j) {
 
 void printAdjMatrix(int arr[V][V]) {
     int i, j;
-    printf("   ");
+    printf("   ");      //space before column
     for (i = 0; i < V; i++) {
-        printf("%d ", i);
+        printf("%d ", i);    //column numbers
     }
     printf("\n");
 
     for (i = 0; i < V; i++) {
-        printf("%d  ", i);
+        printf("%d  ", i);   //row number 
         for (j = 0; j < V; j++) {
-            printf("%d ", arr[i][j]);
+            printf("%d ", arr[i][j]);   //each row fill
         }
-        printf("\n");
+        printf("\n");  //next row
+        
     }
 }
 
@@ -39,7 +40,7 @@ int main() {
     insertEdge(adjMatrix, 0, 1);
     insertEdge(adjMatrix, 0, 2);
     insertEdge(adjMatrix, 0, 3);
-    insertEdge(adjMatrix, 2, 0);
+    insertEdge(adjMatrix, 2, 0); //undirected not present
     insertEdge(adjMatrix, 2, 3);
     insertEdge(adjMatrix, 1, 3);
     insertEdge(adjMatrix, 0, 4);
